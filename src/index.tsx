74b9d4c9f760fe2dbdf3e0fb629a46dc7424a3f0
chaykinks <chaykinks@gmail.com>
2026-03-08 18:11:48 +0500

    добавил браузерроутер в index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/app/app.tsx';
 import {Settings, AuthorizationStatus} from './const';
 
@@ -9,9 +10,11 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      offersCount = {Settings.OffersCount}
-      authorizationStatus={AuthorizationStatus.Auth}
-    />
+    <BrowserRouter>
+      <App
+        offersCount={Settings.OffersCount}
+        authorizationStatus={AuthorizationStatus.NoAuth}
+      />
+    </BrowserRouter>
   </React.StrictMode>
 );
