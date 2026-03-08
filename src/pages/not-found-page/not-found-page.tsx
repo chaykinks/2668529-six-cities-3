2a461c4d735f2fb3d1b47c77a4b0bc3b5d10c5bc
chaykinks <chaykinks@gmail.com>
2026-03-08 17:08:42 +0500

    добавил 404 страницу

---
+++ src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,10 @@
+function NotFoundPage(): JSX.Element {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>404 Not Found</h1>
+      <a href="/">Go to main page</a>
+    </div>
+  );
+}
+
+export default NotFoundPage;
