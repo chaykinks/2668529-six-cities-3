116d912194c531669743298e07452b4bea65b5c2
chaykinks <chaykinks@gmail.com>
2026-03-26 21:51:40 +0500

    заменил получение офферов через props на получение из store

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -8,14 +8,19 @@ import PrivateRoute from '../private-route/private-route';
 import Layout from '../layout/layout.tsx';
 import ScrollToTop from '../../components/scroll-to-top/scroll-to-top';
 import {AuthorizationStatus, AppRoute} from '../../const';
-import {Offer} from '../../types/offer';
+import { offers } from '../../mocks/offers';
+import { AppDispatch } from '../../store';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fillOffers } from '../../store/action';
 
 type AppProps = {
-  offers: Offer[];
   authorizationStatus: AuthorizationStatus;
 };
 
-function App({offers, authorizationStatus}: AppProps): JSX.Element {
+function App({authorizationStatus}: AppProps): JSX.Element {
+  const dispatch = useDispatch<AppDispatch>();
+  useEffect(() => {dispatch(fillOffers(offers))}, [dispatch]);
 
   return (
     <BrowserRouter>
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,4 +4,4 @@ import { reducer } from './reducer';
 export const store = configureStore({reducer});
 
 //export type State = ReturnType<typeof store.getState>;
-//export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch;
