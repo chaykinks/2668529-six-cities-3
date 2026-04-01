a8940157c66660ba367895e24357a78045c2bef3
chaykinks <chaykinks@gmail.com>
2026-04-01 22:25:36 +0500

    добавил модуль api.ts

---
+++ src/api.ts
@@ -0,0 +1,14 @@
+import axios, { AxiosInstance } from 'axios';
+
+const REQUEST_TIMEOUT = 5000;
+
+function createAPI(): AxiosInstance {
+  const api = axios.create({
+    baseURL: 'https://15.design.htmlacademy.pro/six-cities',
+    timeout: REQUEST_TIMEOUT,
+  });
+
+  return api;
+}
+
+export { createAPI };
